1bde80892e2c450763f483fd24f93eaf44f3af35
Beth Haubert <bethany.haubert@gmail.com>
2016-03-22 16:29:43 -0500

    working on default client id value

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   def dashboard
     @list_clients = Client.all
-    @client = Client.find_by_id(1)
-    render "dashboard"
+    @client = Client.find_by_id(params[:client_id])
+    render "dashboard/#{@client.id}"
   end
 end
