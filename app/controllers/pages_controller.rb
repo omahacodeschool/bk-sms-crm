7d0f83993a220251b880c34e2bbfe235c4c1e4b3
Sumeet Jain <sumeet@sumeetjain.com>
2016-03-22 16:38:26 -0500

    Conditionally set @client, based on params[:client_id]

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,13 @@
 class PagesController < ApplicationController
   def dashboard
     @list_clients = Client.all
-    @client = Client.find_by_id(params[:client_id])
+
+    if client_from_params = Client.find_by_id(params[:client_id])
+      @client = client_from_params
+    else
+      @client = Client.first
+    end
+    
     render "dashboard/#{@client.id}"
   end
 end
