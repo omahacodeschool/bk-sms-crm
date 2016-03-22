bb20537670d7833b18b5356692651d4a6fca8acf
Jeremy Stoll <j8r8my@gmail.com>
2016-03-22 11:47:32 -0500

    Merging with updated  Master

--- app/controllers/touch_controller.rb
+++ app/controllers/touch_controller.rb
@@ -1,5 +1,12 @@
 class TouchController < ApplicationController
   
+  def create_new
+    x = Touch.new
+    x.business_id = (params[:business_id])
+    x.client_id = (params[:client_id])
+    x.message = (params[:message_content])
+    x.outgoing = true
+    x.read = true
+  end
 
-  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,3 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
 end
+
+Rails.application.routes.draw do
+  post "touch/create_new" => "touch#create_new"
+end
