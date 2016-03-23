e23229e10081d6a4bc4cbc114b666e25c0b05a3f
Jeremy Stoll <j8r8my@gmail.com>
2016-03-23 11:17:29 -0500

    In Progress

--- app/models/touch.rb
+++ app/models/touch.rb
@@ -1,8 +1,10 @@
 class Touch < ActiveRecord::Base
   belongs_to :client
   belongs_to :user
+
   def self.search(search)
   # where("name LIKE ?", "%#{search}%") 
   where("message LIKE ?", "%#{search}%")
   end
+  
 end
