d459b3d1f957dbd10528775fffa5f1058e344d82
Sumeet Jain <sumeet@sumeetjain.com>
2016-03-24 16:51:51 -0500

    Use PG Search gem to do a multi-table search. It rocks.

--- app/controllers/touch_controller.rb
+++ app/controllers/touch_controller.rb
@@ -17,54 +17,10 @@ class TouchController < ApplicationController
     @touches = Touch.all
     render "touches"
   end
-  def search_touch
-    # business = user.business
-    # client = business.client         
-    # touches = client.touches
 
-    user = current_user
-    business = Business.where("user_id" => user.id)
+  def search_touch
+    @results = PgSearch.multisearch(params[:search])
     
-    client = []
-    business.each do |business|
-    client<< Client.where("business_id" => business.id)
-    end
-    #
-    #client is an array with AR relations (that businesses clients)
-    #
-    touched = []
-    business.each do |business|
-    touched << Touch.where("business_id" => business.id)
-    end
-    #
-    #touched is an array with AR relations (that businesses touches)
-    #
-    #looping each array (giving ind. objects) through a where statement
-    #
-    touched.each do |touched|
-    @touch = touched.where("message LIKE '%#{params[:search]}%'") 
-    end
-    client.each do |client|
-    @first_name = client.where("first_name LIKE '%#{params[:search]}%'")
-    end
-    client.each do |client|
-    @last_name = client.where("last_name LIKE '%#{params[:search]}%'")
-    end
-    client.each do |client|
-    @phone_number = client.where("phone_number LIKE '%#{params[:search]}%'")
-    end
-    client.each do |client|
-    @notes = client.where("notes LIKE '%#{params[:search]}%'")
-    end
     render "search"
   end
 end
-
-
-
-
-
-
-
-
-
