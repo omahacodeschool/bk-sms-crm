ff0109e9b811f88af5c8ed4050deda5f18036648
Sumeet Jain <sumeet@sumeetjain.com>
2016-03-24 16:37:33 -0500

    Add 'multisearchable :against...' to models that are searchable

--- app/models/touch.rb
+++ app/models/touch.rb
@@ -1,14 +1,12 @@
 class Touch < ActiveRecord::Base
   belongs_to :client
   include PgSearch
+
+  multisearchable :against => [:message]
+  
   def self.search(search)
   # where("name LIKE ?", "%#{search}%") 
   where("message LIKE ?", "%#{search}%")
   end
   
-end
-
-
-class Shape < ActiveRecord::Base
-  include PgSearch
 end
