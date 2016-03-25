5347e22cb132e69087f3959dd2820c9a8976ff40
Jeremy Stoll <j8r8my@gmail.com>
2016-03-24 20:14:53 -0500

    Connected Twilio API, still testing.

--- app/controllers/touch_controller.rb
+++ app/controllers/touch_controller.rb
@@ -1,4 +1,5 @@
 class TouchController < ApplicationController
+  require 'twilio-ruby'
   
   def create_new
     x = Touch.new
@@ -55,4 +56,34 @@ class TouchController < ApplicationController
 
     render "search"
   end
+
+
+  # Method takes a Touch object and its ID... and creates a text message from the data saved in DB.
+  #returns nil.
+  def send_sms(id)
+    
+    @text_to_send = Touch.find_by_id(id)  
+    @text_recipient = Client.find_by_id(@text_to_send.client_id)
+    @text_content = @text_to_send.message
+    @text_sender_business = Business.find_by_id(@text_recipient.business_id)
+    # credentials:
+
+    account_sid = 'AC7f31bedb178e5c29681712d702181f29' 
+    auth_token = '0b67f2ae87cae02e784c98378a806368' 
+
+    # set up a client to talk to the Twilio REST API 
+    @client = Twilio::REST::Client.new(account_sid, auth_token) 
+     
+    # Sending an SMS:
+
+    @client.account.messages.create(
+      :from => @text_sender_business.business_phone, 
+      :to => @text_recipient.phone_number, 
+      :body => @text_content 
+
+      # UNCOMMENT THIS (AND ADD SOME FUNCTIONALITY, PERHAPS) FOR MULTIMEDIA MESSAGE:
+      # , :media_url => 'http://farm2.static.flickr.com/1075/1404618563_3ed9a44a3a.jpg'
+    )
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,28 +7,10 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 x = User.new
-x.phone_number = "1111111111"
+x.phone_number = "7777777777" 
 x.email ="user@gmail.com"
 x.password = "password"
 x.save
-
-# /////////////////////////////////////////////////
-
-y = Client.new
-y.first_name = "Lily"
-y.last_name = "Tomlin"
-y.notes = "Likes to buy stuff"
-y.active = true
-y.phone_number = "6666666666"
-y.save
-
-a = Client.new
-a.first_name = "John"
-a.last_name = "Updike"
-a.notes = "Just a guy"
-a.active = true
-a.phone_number = "1111111111"
-a.save
 
 # /////////////////////////////////////////////////
 
@@ -41,7 +23,7 @@ b.save
 r = Business.new
 r.user_id = x.id
 r.name = "Real-Bearded Santa Exotic Dancers"
-r.business_phone = "2222222222"
+r.business_phone = "+14028502532"
 r.save
 
 e = Business.new
@@ -58,20 +40,40 @@ k.save
 
 # /////////////////////////////////////////////////
 
+# Client(y) Contains Jeremy's actual phone number. 
+# Feel free to test between 7am and 9pm.  
+y = Client.new
+y.first_name = "Jeremy"
+y.last_name = "Stoll"
+y.notes = "Likes to buy things that are pink or involving cats"
+y.active = true
+y.phone_number = "+14022133739"
+y.business_id = r.id
+y.save
+
+a = Client.new
+a.first_name = "John"
+a.last_name = "Updike"
+a.notes = "Just a guy"
+a.active = true
+a.phone_number = "1111111111"
+a.business_id = b.id
+a.save
+
+# /////////////////////////////////////////////////
+
 z = Touch.new
 z.client_id = y.id
-z.message = "Yes, we do sell things."
+z.message = "Yes, we do sell pink things."
 z.outgoing = true
 z.read = false
-z.business_id = b.id
 z.save
 
 n = Touch.new
 n.client_id = y.id
-n.message = "May we schedule a demo?"
+n.message = "May we schedule a cat calendar demo?"
 n.outgoing = true
 n.read = false
-n.business_id = r.id
 n.save
 
 u = Touch.new
@@ -79,7 +81,6 @@ u.client_id = a.id
 u.message = "I need some things you sell."
 u.outgoing = false
 u.read = false
-u.business_id = e.id
 u.save
 
 w = Touch.new
@@ -87,5 +88,4 @@ w.client_id = a.id
 w.message = "I'd love to buy those things!"
 w.outgoing = false
 w.read = true
-w.business_id = k.id
 w.save
