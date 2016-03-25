daae8269c737e16a4a92cd363f1f42a97bed8583
Sumeet Jain <sumeet@sumeetjain.com>
2016-03-25 14:32:23 -0500

    Use XHR to get the HTML for the thread view

--- app/assets/javascripts/load_client_thread.js
+++ app/assets/javascripts/load_client_thread.js
@@ -3,6 +3,14 @@ window.onload = function(){
   var center = document.getElementById("center");
 
   client.addEventListener("click", function(e){
-    center.innerHTML = "Foo";
+    var touches_request = new XMLHttpRequest();
+
+    touches_request.open("GET", "/touch/current_thread/1");
+
+    touches_request.addEventListener("load", function(request_object){
+      alert(request_object.target.response);
+    });
+
+    touches_request.send();
   })
 };
