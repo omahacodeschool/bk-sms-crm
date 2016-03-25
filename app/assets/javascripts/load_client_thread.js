5908a2e9b9243f4938905d39f01fae169b424bc4
Sumeet Jain <sumeet@sumeetjain.com>
2016-03-25 14:42:52 -0500

    Comment out starter code for getting it to work with multiple clients

--- app/assets/javascripts/load_client_thread.js
+++ app/assets/javascripts/load_client_thread.js
@@ -1,16 +1,21 @@
 window.onload = function(){
-  var client = document.getElementById("mike");
+  var clients = document.getElementsByClassName("client");
   var center = document.getElementById("center");
 
-  client.addEventListener("click", function(e){
-    var touches_request = new XMLHttpRequest();
+  // loop through clients
+  //   for each
+  //       addEventListener like below:
+              // var client_id = this element (this client)'s value for the "data-client-id" attribute
 
-    touches_request.open("GET", "/touch/current_thread/1");
+  //         client.addEventListener("click", function(){
+  //           var touches_request = new XMLHttpRequest();
 
-    touches_request.addEventListener("load", function(request_object){
-      center.innerHTML = request_object.target.response;
-    });
+  //           touches_request.open("GET", "/touch/current_thread/" + client_id);
 
-    touches_request.send();
-  })
+  //           touches_request.addEventListener("load", function(request_object){
+  //             center.innerHTML = request_object.target.response;
+  //           });
+
+  //           touches_request.send();
+  //         })
 };
