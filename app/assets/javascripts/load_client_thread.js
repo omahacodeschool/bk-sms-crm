afd62e9bb0c86f365eaf9998b13653ad1ebfa86f
Beth Haubert <bethany.haubert@gmail.com>
2016-03-25 15:42:21 -0500

    js

--- app/assets/javascripts/load_client_thread.js
+++ app/assets/javascripts/load_client_thread.js
@@ -7,6 +7,25 @@ window.onload = function(){
   //       addEventListener like below:
               // var client_id = this element (this client)'s value for the "data-client-id" attribute
 
+// beth added
+
+
+//   @clients.each do |client|
+  
+//         client.addEventListener("load", function(){
+//           var client_id = getElementByAttribute("data-client-id").value;
+//         })
+              
+
+
+
+
+
+
+
+
+
+
   //         client.addEventListener("click", function(){
   //           var touches_request = new XMLHttpRequest();
 
