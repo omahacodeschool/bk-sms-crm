e3640a688ef59685e4bd3dc8aa313221fe8ac698
Michael Meier <mmeier61@yahoo.com>
2016-03-28 12:48:44 -0500

    ok

--- app/assets/javascripts/load_client_thread.js
+++ app/assets/javascripts/load_client_thread.js
@@ -1,45 +1,33 @@
 window.onload = function(){
-  var client = document.getElementsByClassName("client");
+  var client = document.getElementsByClassName('client');
   var center = document.getElementById("center");
-  alert("this works now");
+
+  
+
+  
   // loop through clients
   //   for each
   //       addEventListener like below:
               // var client_id = this element (this client)'s value for the "data-client-id" attribute
-
-
+              
+          for (i =0; i < client.length; i++){
+            var client_id = client[i].dataset.client};
           client.addEventListener("click", function(){
             var touches_request = new XMLHttpRequest();
-
-// beth added
-
-
-//   @clients.each do |client|
-  
-//         client.addEventListener("load", function(){
-//           var client_id = getElementByAttribute("data-client-id").value;
-//         })
-              
-
-
-
-
-
-
-
-
-
-
-  //         client.addEventListener("click", function(){
-  //           var touches_request = new XMLHttpRequest();
-
-
-            touches_request.open("GET", "/touch/current_thread/" + client_id);
-
+            touches_request.open("GET", "message/" + client_id);
             touches_request.addEventListener("load", function(request_object){
-              center.innerHTML = request_object.target.response;
-            });
+            center.innerHTML = request_object.target.response;
+             });
 
             touches_request.send();
           })
 };
+// beth added
+//   @clients.each do |client|
+//         client.addEventListener("load", function(){
+//           var client_id = getElementByAttribute("data-client-id").value;
+//         })
+  //         client.addEventListener("click", function(){
+  //           var touches_request = new XMLHttpRequest();
+
+            
