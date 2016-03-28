b56fa067655836e409860834fb9240143f6aa6e3
Sumeet Jain <sumeet@sumeetjain.com>
2016-03-28 13:09:48 -0500

    Fix indentation and clear up code comments

--- app/assets/javascripts/load_client_thread.js
+++ app/assets/javascripts/load_client_thread.js
@@ -2,33 +2,24 @@ window.onload = function(){
   var clients = document.getElementsByClassName('client');
   var center = document.getElementById("center");
 
-  
+  // loop through clients            
+  for (i =0; i < clients.length; i++){
 
-  
-  // loop through clients
-  //   for each
-  //       addEventListener like below:
-              // var client_id = this element (this client)'s value for the "data-client-id" attribute
-              
-          for (i =0; i < clients.length; i++){
-            var client = clients[i];
-            var client_id = client.dataset.client};
-          clients.addEventListener("click", function(){
-            var touches_request = new XMLHttpRequest();
-            touches_request.open("GET", "message/" + client_id);
-            touches_request.addEventListener("load", function(request_object){
-            center.innerHTML = request_object.target.response;
-             });
+    // for each DOM element (represented by 'client')...
+    var client = clients[i];
+    
+    // this client's value for the "data-client-id" attribute
+    var client_id = client.dataset.client;
 
-            touches_request.send();
-          })
+    // When a client is clicked, load its info
+    clients.addEventListener("click", function(){
+      var touches_request = new XMLHttpRequest();
+      touches_request.open("GET", "message/" + client_id);
+      touches_request.addEventListener("load", function(request_object){
+        center.innerHTML = request_object.target.response;
+      });
+
+      touches_request.send();
+    });
+  }
 };
-// beth added
-//   @clients.each do |client|
-//         client.addEventListener("load", function(){
-//           var client_id = getElementByAttribute("data-client-id").value;
-//         })
-  //         client.addEventListener("click", function(){
-  //           var touches_request = new XMLHttpRequest();
-
-            
