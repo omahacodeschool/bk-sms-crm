9b20650d0669c4e0fe216f77d0bba2024c43281d
Sumeet Jain <sumeet@sumeetjain.com>
2016-03-29 10:45:59 -0500

    Use event.target instead of referring to 'client' variable

--- app/assets/javascripts/load_client_thread.js
+++ app/assets/javascripts/load_client_thread.js
@@ -5,9 +5,9 @@ window.onload = function(){
 
   for (i =0; i < clients.length; i++){
     var client = clients[i];
-    var client_id = client.dataset.clientId;
 
-    client.addEventListener("click", function(){
+    client.addEventListener("click", function(event){
+      var client_id = event.target.dataset.clientId;
       var touches_request = new XMLHttpRequest();
       var client_request = new XMLHttpRequest();
       touches_request.open("GET", "message/" + client_id);
