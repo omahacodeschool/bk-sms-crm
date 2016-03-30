ad2a4a8c99cda9f7cdc65973855ff8336cd89821
Beth Haubert <bethany.haubert@gmail.com>
2016-03-30 11:31:53 -0500

    deleted old stuff and changed redirect

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
     @business = current_user.businesses.find_by_id(params[:id])
 
     if @business.nil?
-      redirect_to("/")
+      redirect_to("/businesses/view")
     else
       @clients = @business.clients
 
@@ -15,32 +15,4 @@ class PagesController < ApplicationController
     end
   end
 
-  def dashboard
-    touch = 1
-    user = current_user
-    @business = Business.find_by_id(params[:id])
-    @client = Client.where("business_id" => @business.id)
-    client.each do |client|
-    @touch = Touch.where("client_id" => client.id)
-    end
-    @incoming = @touch.where("outgoing" => false)
-    @outgoing = @touch.where("outgoing" => true)
-    ############################################################
-    @list_clients = Client.where({"active" => true})
-    @touches = Touch.all
-    if client_from_params = Client.find_by_id(params[:client_id])
-      @client = client_from_params
-    else
-      @client = nil
-    end
-
-    # The view being rendered is not dependent on the value of @client.
-    # It's just the view located at views/pages/dashboard.html.erb.
-    render "pages/dashboard"
-  end
-
-  def onboard
-    @user = current_user
-    render "pages/onboard"
-  end
 end
