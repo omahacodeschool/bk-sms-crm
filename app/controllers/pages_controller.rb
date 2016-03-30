641fbac8aa5b5d792ce8ea66e94e4927dcc4e0b3
Sumeet Jain <sumeet@sumeetjain.com>
2016-03-30 11:26:39 -0500

    Restrict dashboard to logged in users' businesses only

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,17 @@
 class PagesController < ApplicationController
   def dashboard_business
-    current_user
-    @business = Business.find_by_id(params[:id])
-    @clients = @business.clients
+    @business = current_user.businesses.find_by_id(params[:id])
 
-    if Touch.where({"client_id" => @current_client}) != nil
-      @touches = Touch.where({"client_id" => @current_client})
+    if @business.nil?
+      redirect_to("/")
     else
-      @touches = nil
+      @clients = @business.clients
+
+      if Touch.where({"client_id" => @current_client}) != nil
+        @touches = Touch.where({"client_id" => @current_client})
+      else
+        @touches = nil
+      end
     end
   end
 
