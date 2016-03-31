5da7637133e9fb657bc2ac29e0320599377cb573
Beth Haubert <bethany.haubert@gmail.com>
2016-03-31 16:20:39 -0500

    reverted back to original

--- app/assets/javascripts/display_selected_client_from_dropdown.js
+++ app/assets/javascripts/display_selected_client_from_dropdown.js
@@ -1,29 +1,29 @@
 jQuery(document).ready(function(){
   var parent = document.getElementById("right");
-  // parent.addEventListener("click", function(event){
-  var dropdown_select = document.getElementById("dropdown_select");
 
-    dropdown_select.addEventListener("click", function(event){
-      var selected_client = event.target.options[event.selectedIndex];
-      var presentation_area = document.getElementById("current_client");
-      var client_id = selected_client.value;
+  parent.addEventListener("click", function(event){
 
+    if (event.target.matches("#dropdown_submit")){
+      var selectedId = document.getElementById("dropdown_select");
 
-      var request = new XMLHttpRequest();
-      var request_path = "/clients/profile/"  + client_id;
+        var presentation_area = document.getElementById("current_client");
+        var client_id = selectedId.options[selectedId.selectedIndex].value;
 
-      request.open("GET", request_path);
+        var request = new XMLHttpRequest();
+        var request_path = "/clients/profile/"  + client_id;
 
-        request.addEventListener("load", function(request_object){
-          presentation_area.innerHTML = request_object;
-        });
+        request.open("GET", request_path);
 
-      request.send();
+          request.addEventListener("load", function(request_object){
+            presentation_area.innerHTML = request_object.target.response;
+          });
 
-      event.preventDefault();
+        request.send();
+
+        event.preventDefault();
        
-      });
-    // });
+      };
+    });
 
-  });
+});
 
