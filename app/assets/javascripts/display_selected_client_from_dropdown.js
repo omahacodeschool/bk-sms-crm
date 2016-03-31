81bb8a916dfa2a55b6b9a74a515653223d5b0034
Beth Haubert <bethany.haubert@gmail.com>
2016-03-31 16:33:45 -0500

    js has working code and non-working code commented

--- app/assets/javascripts/display_selected_client_from_dropdown.js
+++ app/assets/javascripts/display_selected_client_from_dropdown.js
@@ -1,3 +1,4 @@
+// WORKING BUT REQUIRES SUBMIT BUTTON
 jQuery(document).ready(function(){
   var parent = document.getElementById("right");
 
@@ -27,3 +28,27 @@ jQuery(document).ready(function(){
 
 });
 
+// NOT WORKING BUT DOESN'T REQUIRE SUBMIT BUTTON
+
+// jQuery(document).ready(function(){
+//   var dropdown = document.getElementById("dropdown_select");
+
+//   dropdown.addEventListener("change", function(event){
+
+//     var presentation_area = document.getElementById("current_client");
+//     var client_id = dropdown.options[dropdown.selectedIndex].value;
+
+//     var request = new XMLHttpRequest();
+//     var request_path = "/clients/profile/"  + client_id;
+
+//     request.open("GET", request_path);
+
+//       request.addEventListener("load", function(request_object){
+//         presentation_area.innerHTML = request_object.target.response;
+//       });
+
+//     request.send();
+
+//     event.preventDefault();
+//   });
+// };
