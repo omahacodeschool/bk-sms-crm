bb77ad8f74a364c43526ae15fb4f3a895a09bdbd
Sumeet Jain <sumeet@sumeetjain.com>
2016-03-31 17:03:05 -0500

    Start a Touch#smart_time_date method

--- app/models/touch.rb
+++ app/models/touch.rb
@@ -3,4 +3,16 @@ class Touch < ActiveRecord::Base
   include PgSearch
 
   multisearchable :against => [:message]
+
+  # Return either today's time, "yesterday", or a past date--depending on
+  # when this Touch was created relative to today.
+  def smart_time_date
+    # if self.created_at is from today
+    #   Just the time of self's created_at
+    # elsif self.created_at is from yesterday
+    #   Just "yesterday"
+    # else
+    #   The date (03/12/16) of self.created_at
+    # end
+  end
 end
