e9fd8ed1b43c3eb11ffa16caf0cc1e1ffff2dc14
Jeremy Stoll <j8r8my@gmail.com>
2016-04-01 15:32:40 -0500

    No errors, close to success.

--- app/assets/javascripts/load_client_thread.js
+++ app/assets/javascripts/load_client_thread.js
@@ -9,10 +9,10 @@ jQuery(document).ready(function(){
     var client = clients[i];
 
     client.addEventListener("click", function(event){
-      var client_id = event.target.dataset.clientId;
-      // event.target.style.backgroundColor = "gray";
-      // event.target.style.color = "white";
-      
+      client_id = event.target.dataset.clientId;
+
+      // Make each ^ (client_id) available to SUBMIT BUTTON EL.
+
       var touches_request = new XMLHttpRequest();
       var client_request = new XMLHttpRequest();
       touches_request.open("GET", "message/" + client_id);
@@ -23,19 +23,33 @@ jQuery(document).ready(function(){
         
         thread_display.innerHTML = request_object.target.response;
         thread_display.style.display = "block";
-          
+
+
+        // This is the SUBMIT Button EL.
+        // It needs access to client_id, and sends out an XHR to
+        // "send_message/" + client_id.
+        // The controller SAVES msg, SENDS msg, and redirects to
+        // "message/" + client_id
+        // The response ( event.target.response) gets put into the innerHTML
+        // and is rendered on the page. 
+
+        var current_client = document.getElementById("current_client");
+          debugger;
         var submit_button = document.getElementById("send_message_button")
         submit_button.addEventListener("click", function(){
           alert("ALERTED")
-        });
+          debugger;
 
-      }); //End of EVENT LISTENER?
+                  });
+
+
+      }); //End of SUBMIT BUTTON EVENT LISTENER?
 
       client_request.addEventListener("load", function(request_object){
         client_display.innerHTML = request_object.target.response;
         client_display.style.display = "block";
 
-      }); //End of EVENT LISTENER?
+      }); //End of CLIENT REQUEST EVENT LISTENER?
 
 
       touches_request.send();
