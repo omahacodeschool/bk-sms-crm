43a1084f4d4a19171ba0561a7279bd33cde2a8ab
Beth Haubert <bethany.haubert@gmail.com>
2016-04-01 16:26:58 -0500

    working

--- app/assets/javascripts/display_selected_client_from_dropdown.js
+++ app/assets/javascripts/display_selected_client_from_dropdown.js
@@ -1,16 +1,17 @@
-// WORKING BUT REQUIRES SUBMIT BUTTON
 jQuery(document).ready(function(){
-  var parent = document.getElementById("right");
+  // var parent = document.getElementById("right");
+  var dropdown = document.getElementById("dropdown_select");
   var thread_display = document.getElementById("thread");
-  var client_id = ""
 
-  parent.addEventListener("click", function(event){
+  // parent.addEventListener("click", function(event){
+  dropdown.addEventListener("change", function(event){
 
-    if (event.target.matches("#dropdown_submit")){
-      var selectedId = document.getElementById("dropdown_select");
+    // if (event.target.matches("#dropdown_submit")){
+      // var selectedId = document.getElementById("dropdown_select");
 
         var presentation_area = document.getElementById("current_client");
-        var client_id = selectedId.options[selectedId.selectedIndex].value;
+        // var client_id = selectedId.options[selectedId.selectedIndex].value;
+        var client_id = dropdown.options[dropdown.selectedIndex].value;
 
         var touches_request = new XMLHttpRequest();
         var client_request = new XMLHttpRequest();
@@ -34,33 +35,7 @@ jQuery(document).ready(function(){
 
         event.preventDefault();
        
-      };
+      });
     });
 
-});
-
-// NOT WORKING BUT DOESN'T REQUIRE SUBMIT BUTTON
-
-// jQuery(document).ready(function(){
-//   var dropdown = document.getElementById("dropdown_select");
-
-//   dropdown.addEventListener("change", function(event){
-
-//     var presentation_area = document.getElementById("current_client");
-//     var client_id = dropdown.options[dropdown.selectedIndex].value;
-
-//     var request = new XMLHttpRequest();
-//     var request_path = "/clients/profile/"  + client_id;
-
-//     request.open("GET", request_path);
-
-//       request.addEventListener("load", function(request_object){
-//         presentation_area.innerHTML = request_object.target.response;
-//       });
-
-//     request.send();
-
-//     event.preventDefault();
-//   });
-// };
 
