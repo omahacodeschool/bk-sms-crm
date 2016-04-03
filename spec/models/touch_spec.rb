0c893214592ad82d01f18d98ab067ea5c117174b
Beth Haubert <bethany.haubert@gmail.com>
2016-04-03 14:45:07 -0500

    working-ish but will be issues because created_at constantly changing depending on when you run test

--- spec/models/touch_spec.rb
+++ spec/models/touch_spec.rb
@@ -1,6 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Touch, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-  
+
+  describe "#smart_time_date" do
+    it "was sent before yesterday" do
+      touch = Touch.new(created_at: Time.now)
+      assert_equal("04/03/16", touch.smart_time_date)
+    end
+  end
+
+  # describe "#smart_time_date" do
+  #   it "was sent yesterday" do
+  #     touch = Touch.new(created_at: 86500.seconds.ago)
+  #     assert_equal("04/03/16", touch.smart_time_date)
+  #   end
+  # end
+
+  # describe "#smart_time_date" do
+  #   it "was sent before yesterday" do
+  #     touch = Touch.new(created_at: Time.now)
+  #     assert_equal("04/03/16", touch.smart_time_date)
+  #   end
+  # end
+
 end
