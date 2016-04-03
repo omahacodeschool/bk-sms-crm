ee608dffff714f696f4d3385de43ea5ba9cf7e97
Beth Haubert <bethany.haubert@gmail.com>
2016-04-03 13:14:02 -0500

    refactor

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,14 +5,6 @@ class PagesController < ApplicationController
     @clients = @business.clients
     if @business.nil?
       redirect_to("/businesses/view")
-    # else
-    #   @clients = []
-    #   @business.clients.each do |client|
-    #     if Touch.where({"client_id" => @current_client}) != nil
-    #       @clients << client
-    #     end
-    #   end
-    #   return @clients
     end
   end
 end
