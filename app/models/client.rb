c7f8537c206d3571cac7deeff4c1e7ed4354dbab
Beth Haubert <bethany.haubert@gmail.com>
2016-04-03 13:38:05 -0500

    removed date method from client controller, used touch method in dashboard

--- app/models/client.rb
+++ app/models/client.rb
@@ -6,29 +6,11 @@ class Client < ActiveRecord::Base
   multisearchable :against => [:first_name, :last_name, :phone_number, :notes]
 
   
-   #Finds all messages and puts in them in order of their creation
+   #Finds all Touch objects and puts in them in order of their creation
    #
    #Returns the first Touch object
    def most_recent_message
-    self.all_sent_messages.order("created_at DESC").first
+    self.touches.order("created_at DESC").first
    end
-
-   #Loops through all incoming Touches to get the time stamps
-   #
-   #Returns a String
-   def date
-    time_stamp = ""
-    all_sent_messages.each do |p|
-      time_stamp = p.created_at.strftime("%m/%d/%y")
-    end
-    return time_stamp
-   end
-
-   #Finds the incoming Touch objects of a specific client
-   #
-   #Returns an Active Record realtion
-   def all_sent_messages
-    Touch.where("client_id = ? and outgoing = ?", self.id, false) 
-  end
 
 end
