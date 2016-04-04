b29eae4013253277ca378486f597dc45239ed1e6
Michael Meier <mmeier61@yahoo.com>
2016-04-04 09:28:11 -0500

    test

--- spec/models/touch_spec.rb
+++ spec/models/touch_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe Touch, type: :model do
   # describe "#smart_time_date" do
   #   context "when the Touch's created_at date is today" do
   #   it "displays the time that the Touch's created_at" do
-  #     touch = Touch.new(created_at: "Today.now")
-  #     assert_equal("Today.now.strftime("%I:%M")", touch.smart_time_date)
+  #     touch = Touch.new(created_at: "Time.now")
+  #     assert_equal("09:23", touch.smart_time_date)
   #   end
 
   #   context "when the Touch's created_at date is yesterday" do
@@ -18,16 +18,16 @@ RSpec.describe Touch, type: :model do
   #   context "when the Touch's created_at date any thing else"
   #   it "displays the date of the Touch's created_at" do
   #     touch = Touch.new(created_at: "2.day.ago") #this actually returns that
-  #     assert_equal("2.day.ago.strftime("%m/%d/%y")", touch.smart_time_date)
+  #     assert_equal("04/02/16", touch.smart_time_date)
   #   end 
 
 
-  describe "#smart_time_date" do
-    it "was sent before yesterday" do
-      touch = Touch.new(created_at: Time.now)
-      assert_equal("04/03/16", touch.smart_time_date)
-    end
-  end
+  # describe "#smart_time_date" do
+  #   it "was sent before yesterday" do
+  #     touch = Touch.new(created_at: Time.now)
+  #     assert_equal("04/03/16", touch.smart_time_date)
+  #   end
+  # end
 
   # describe "#smart_time_date" do
   #   it "was sent yesterday" do
@@ -44,4 +44,4 @@ RSpec.describe Touch, type: :model do
   # end
 
 
-end
+
