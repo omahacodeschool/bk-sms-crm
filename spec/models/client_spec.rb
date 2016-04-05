68ae733466cdcee46623a69039e5a76a2e3a3ab4
Beth Haubert <bethany.haubert@gmail.com>
2016-04-05 14:25:24 -0500

    working on again

--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -5,9 +5,11 @@ RSpec.describe Client, type: :model do
    describe "#most_recent_message" do
     it "was the most recent message" do
       client = Client.new(id: 1)
-      touch1 = Touch.new(client_id: 1, created_at: 10.seconds.ago)
-      touch2 = Touch.new(client_id: 1, created_at: 5.seconds.ago)
-      touch3 = Touch.new(client_id: 1, created_at: Time.now)
+      @time_now = Time.parse("Apr 05 2016")
+      Time.stub!(:now).and_return(@time_now)
+      touch1 = Touch.new(client_id: 1, created_at: Time.now + 5)
+      touch2 = Touch.new(client_id: 1, created_at: Time.now + 10)
+      touch3 = Touch.new(client_id: 1, created_at: Time.now + 20)
       assert_equal(touch3, client.most_recent_message)
     end
   end
--- spec/models/touch_spec.rb
+++ spec/models/touch_spec.rb
@@ -2,44 +2,46 @@ require 'rails_helper'
 
 RSpec.describe Touch, type: :model do
 
-  # describe "#smart_time_date" do
-  #   context "when the Touch's created_at date is today" do
-  #   it "displays the time that the Touch's created_at" do
-  #     touch = Touch.new(created_at: "Today.now")
-  #     assert_equal("Today.now.strftime("%I:%M")", touch.smart_time_date)
-  #   end
-
-  #   context "when the Touch's created_at date is yesterday" do
-  #   it "displays 'Yesterday'" do
-  #     touch = Touch.new(created_at: "Date.today.prev_day")
-  #     assert_equal("Yesterday", touch.smart_time_date)
-  #   end 
-
-  #   context "when the Touch's created_at date any thing else"
-  #   it "displays the date of the Touch's created_at" do
-  #     touch = Touch.new(created_at: "2.day.ago") #this actually returns that
-  #     assert_equal("2.day.ago.strftime("%m/%d/%y")", touch.smart_time_date)
-  #   end 
-
-
   describe "#smart_time_date" do
-    it "was sent before yesterday" do
-      touch = Touch.new(created_at: Time.now)
-      assert_equal("04/03/16", touch.smart_time_date)
+    context "when the Touch's created_at date is today" do
+    it "displays the time that the Touch's created_at" do
+      touch = Touch.new(created_at: "Today.now")
+      assert_equal("Today.now.strftime("%I:%M")", touch.smart_time_date)
     end
-  end
+
+    context "when the Touch's created_at date is yesterday" do
+    it "displays 'Yesterday'" do
+      touch = Touch.new(created_at: "Date.today.prev_day")
+      assert_equal("Yesterday", touch.smart_time_date)
+    end 
+
+    context "when the Touch's created_at date any thing else"
+    it "displays the date of the Touch's created_at" do
+      touch = Touch.new(created_at: "2.day.ago") #this actually returns that
+      assert_equal("2.day.ago.strftime("%m/%d/%y")", touch.smart_time_date)
+    end 
+
+
+  # describe "#smart_time_date" do
+  #   it "was sent before yesterday" do
+  #     @time_now = Time.parse("Apr 03 2016")
+  #     Time.stub!(:now).and_return(@time_now)
+  #     touch = Touch.new(created_at: Time.now)
+  #     assert_equal("04/03/16", touch.smart_time_date)
+  #   end
+  # end
 
   # describe "#smart_time_date" do
   #   it "was sent yesterday" do
   #     touch = Touch.new(created_at: 86500.seconds.ago)
-  #     assert_equal("04/03/16", touch.smart_time_date)
+  #     assert_equal("Today", touch.smart_time_date)
   #   end
   # end
 
   # describe "#smart_time_date" do
   #   it "was sent before yesterday" do
   #     touch = Touch.new(created_at: Time.now)
-  #     assert_equal("04/03/16", touch.smart_time_date)
+  #     assert_equal("Yesterday", touch.smart_time_date)
   #   end
   # end
 
